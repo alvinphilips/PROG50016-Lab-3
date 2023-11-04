ce4fe8b0863712a8d619fc3c7b47f77905936f68
Alvin Philips <alvinphilips257@gmail.com>
2023-11-04 00:53:45 -0400

    Add Asset

---
+++ lab3/Asset.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#ifndef ASSET_H
+#define ASSET_H
+
+#include "Object.h"
+
+class Asset: public Object {
+	// Not provided in the UML diagram, but needed to allow Assets to be destroyed
+	friend class AssetManager;
+protected:
+	Asset() = default;
+	~Asset() override = default;
+public:
+	void Load() override;
+};
+
+#endif
